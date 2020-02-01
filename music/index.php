fc3d3c3b19708cd1a739d43d9f46a743ecbd1082
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-02-01 15:19:44 +0000

    Pull website into version control

---
+++ music/index.php
@@ -0,0 +1,6 @@
+<?php
+header("Location: https://docs.google.com/spreadsheet/ccc?key=0AoKvJHRINrlbdFNVdElqUlpJZkZzakQ5NEw3QkNEV1E"); /* Redirect browser */
+
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
